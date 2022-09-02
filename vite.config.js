d6385335dbf0a0806377e44de069289de48e8ce8
Luciano Torrano <lucianotorrano.dev@gmail.com>
2022-09-02 16:37:54 -0300

    deploy error fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://LucianoTorrano.github.com/rick-and-morty/",
+  base: "/rick-and-morty/",
   plugins: [react()]
 })
